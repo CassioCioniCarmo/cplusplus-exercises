c463201cfee60f6ac0217cc7eb6be8e1ff553fdc
Cássio Cioni Carmo <cassiocioni@hotmail.com>
2023-03-30 21:18:41 -0300

    Create exercise_1.cpp

---
+++ exercise_1.cpp
@@ -0,0 +1,17 @@
+/**
+    Exercice - 1: Hello World!
+    First python program just to print the phrase "Hello World!"
+    
+    Cassio Cioni Carmo - 03/30/2023
+**/
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() 
+{
+    printf("Hello, World!");
+    return 0;
+}
